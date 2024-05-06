c2004b0c19c203da6c10f29163bf9dd07731e2b9
Volodymyr <35489438+vhuzii@users.noreply.github.com>
2024-05-06 13:46:07 +0300

    Restruct folders

--- src/Mailtrap/DependencyInjection/Extensions/ServiceCollectionExtensions.cs
+++ src/Mailtrap/DependencyInjection/Extensions/ServiceCollectionExtensions.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
 
-namespace Mailtrap.Extensions.DependencyInjection;
+namespace Mailtrap.DependencyInjection.Extensions;
 
 public static class ServiceCollectionExtensions
 {
